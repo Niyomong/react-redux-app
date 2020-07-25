59fd96be55ecb8262a7a043ba4c496ca909f4173
Niyomong <tt910208@gmail.com>
2020-07-25 14:04:12 +0900

    fist commit

---
+++ src/components/events_index.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import _ from 'lodash'
+import { Link } from 'react-router-dom'
+
+import { readEvents } from '../actions'
+
+class EventsIndex extends Component {
+  componentDidMount() {
+    console.log("hi!")
+    this.props.readEvents()
+  }
+
+  renderEvents() {
+    return _.map(this.props.events, event => (
+      <tr key={event.id}>
+        <td>{event.id}</td>
+        <td>
+          <Link to={`/events/${event.id}`}>
+            {event.title}
+          </Link>
+        </td>
+        <td>{event.body}</td>
+      </tr>
+    ))
+  }
+
+  render() {
+
+    const style = {
+      position: "fixed",
+      right: 12,
+      bottom: 12,
+    }
+
+    return (
+        <React.Fragment>
+
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Body</th>
+            </tr>
+          </thead>
+
+          <tbody displayRowCheckbox={false}>
+            {this.renderEvents()}
+          </tbody>
+        </table>
+
+        <Link to ="/events/new">New Event</Link>
+        </React.Fragment>
+
+    )
+  }
+}
+
+const mapStateToProps = state => ({ events: state.events})
+// const mapDispatchToProps = dispatch => ({
+//   increment: () => dispatch(increment()),
+//   decrement: () => dispatch(decrement())
+// })
+
+const mapDispatchToProps = ({ readEvents })
+
+export default connect(mapStateToProps, mapDispatchToProps)(EventsIndex)
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux' //reduxで追加
+import { Provider } from 'react-redux' //reduxで追加
+import thunk from 'redux-thunk'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
 import './index.css';
-import App from './App';
+import reducer from './reducers' //reduxで追加
+import EventsIndex from './components/events_index';
+import EventsNew from './components/events_new';
+import EventsShow from './components/events_show';
 import * as serviceWorker from './serviceWorker';
 
+
+const enhancer = process.env.NODE_ENV === 'development' ? composeWithDevTools(applyMiddleware(thunk)) : applyMiddleware(thunk)
+const store = createStore(reducer, enhancer) // このアプリケーション内のstateがこのstoreに集約される。
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/events/new" component={EventsNew} />
+          <Route path="/events/:id" component={EventsShow} />
+          <Route exact path="/" component={EventsIndex} />
+          <Route exact path="/events" component={EventsIndex} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
